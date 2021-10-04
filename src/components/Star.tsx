99fad0419dc58f20413cf76fcb01b3209f629e59
Kamil Zainal <gh@akza.dev>
2021-10-04 16:32:02 +0100

    Styling tweaks

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -6,5 +6,10 @@ export default function Star({
   selected = false,
   onSelect = (f) => f,
 }: StarProps): JSX.Element {
-  return <FaStar color={selected ? '#6be1e6' : 'grey'} onClick={onSelect} />;
+  return (
+    <FaStar
+      className={selected ? 'ratingStar-selected' : 'ratingStar'}
+      onClick={onSelect}
+    />
+  );
 }
